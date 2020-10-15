826cf79f4c7e0050a9c43d408324a7a95f67430e
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-15 16:53:25 +0300

    fixed the pictures with the help of constraints

--- traffic_lights/ViewController.swift
+++ traffic_lights/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLable: UIImageView!
+    @IBOutlet var yellowLable: UIImageView!
+    @IBOutlet var greenLable: UIImageView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
