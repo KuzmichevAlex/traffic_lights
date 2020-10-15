831cf5808ad9dfb820f2c98a581a5df1de45ac83
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-15 16:26:54 +0300

    Initial Commit

---
+++ traffic_lights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  traffic_lights
+//
+//  Created by Александр Кузьмичев on 15.10.2020.
+//  Copyright © 2020 Александр Кузьмичев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
