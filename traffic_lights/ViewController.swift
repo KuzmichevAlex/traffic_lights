3493aa3a5b37ee47e542103616010a2cd7b208df
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-21 15:35:06 +0300

    Implemented the logic of traffic lights by pressing a button. Screensaver adapted to different screen sizes

--- traffic_lights/ViewController.swift
+++ traffic_lights/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var redLable: UIImageView!
     @IBOutlet var yellowLable: UIImageView!
     @IBOutlet var greenLable: UIImageView!
     
     @IBOutlet var startButton: UIButton!
     
+    let lightOff: CGFloat = 0.3
+     let lightOn: CGFloat = 1
+     var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLable.layer.cornerRadius = 75
+        yellowLable.layer.cornerRadius = 75
+        greenLable.layer.cornerRadius = 75
+        startButton.layer.cornerRadius = 10
+        
+        redLable.alpha = lightOff
+        yellowLable.alpha = lightOff
+        greenLable.alpha = lightOff
     }
 
-
+    @IBAction func startButtonPress() {
+        
+        switch count {
+        case 0:
+            redLable.alpha = lightOn
+            startButton.setTitle("NEXT", for: .normal)
+            count += 1
+        case 1:
+            redLable.alpha = lightOff
+            yellowLable.alpha = lightOn
+            count += 1
+        case 2:
+            yellowLable.alpha = lightOff
+            greenLable.alpha = lightOn
+            count += 1
+        case 3:
+            redLable.alpha = lightOff
+            yellowLable.alpha = lightOff
+            greenLable.alpha = lightOff
+            startButton.setTitle("START", for: .normal)
+            count = 0
+        default:
+            break
+        }
+    }
+    
 }
+
 
